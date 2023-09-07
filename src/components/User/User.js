efd3c908953eec5bbf6a8d1c4107c1b68fa3724f
Francislainy <francislainy.campos@gmail.com>
2023-09-07 11:22:55 +0100

    Move appBar to its own components

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,7 +3,7 @@ import { Avatar, Typography, Box } from "@mui/material";
 import avatarImage from "../../images/avatar.png";
 
 const User = () => {
-    const chat = {
+    const user = {
         name: 'User 1',
         avatar: 'url_to_avatar_image',
         latestMessage: 'Hello, this is the latest message from user 1',
@@ -14,17 +14,17 @@ const User = () => {
             <Box width="100%" height={200} bgcolor="#f0f0f0" position="relative">
                 <Box position="absolute" bottom={100} left="50%" style={{ transform: 'translateX(-50%)' }}>
                     <Typography variant="subtitle1" gutterBottom sx={{ lineHeight: 1.2, color: '#7B1FA2', fontWeight: 'bold', textAlign: 'center', fontSize: '30px' }}>
-                        {chat.name}
+                        {user.name}
                     </Typography>
                     <Typography variant="body2" gutterBottom sx={{ lineHeight: 1.2, color: '#000000DB', fontWeight: 'bold', textAlign: 'center' }}>
-                        {chat.latestMessage}
+                        {user.latestMessage}
                     </Typography>
                     <Typography variant="body2" color="text.secondary" sx={{ lineHeight: 1.2, color: '#000000DB', fontWeight: 'bold', textAlign: 'center' }}>
                         Another line of text
                     </Typography>
                 </Box>
                 <Avatar
-                    alt={chat.name}
+                    alt={user.name}
                     src={avatarImage}
                     sx={{ width: 130, height: 130, border: '2px solid #7B1FA2', position: 'absolute', bottom: -40, left: '50%', transform: 'translateX(-50%)' }}
                 />
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -5,26 +5,12 @@ import Connect from '../screen/ConnectScreen';
 import Live from '../screen/LiveScreen';
 import Me from '../screen/MeScreen';
 import Moments from '../screen/MomentsScreen';
-import {AppBar, Toolbar, Typography, Button} from '@mui/material';
-import {Link as RouterLink} from 'react-router-dom';
+import CustomAppBar from "../components/AppBar";
 
 const Navigation = () => {
     return (
         <Router>
-            <AppBar position="static" style={{background: '#7B1FA2'}}>
-                <Toolbar>
-                    <Typography variant="h6" style={{flexGrow: 1}}>
-                        <RouterLink to="/" style={{textDecoration: 'none', color: 'inherit'}}>
-                            Hello Talk
-                        </RouterLink>
-                    </Typography>
-                    <Button color="inherit" component={RouterLink} to="/moments">Moments</Button>
-                    <Button color="inherit" component={RouterLink} to="/connect">Connect</Button>
-                    <Button color="inherit" component={RouterLink} to="/live">Live</Button>
-                    <Button color="inherit" component={RouterLink} to="/me">Me</Button>
-                </Toolbar>
-            </AppBar>
-
+            <CustomAppBar />
             <Routes>
                 <Route path="/moments" element={<Moments/>}/>
                 <Route path="/connect" element={<Connect/>}/>

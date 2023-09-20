6e91b6d0588df4c1145b5a32c40be475f9221f5a
Francislainy <francislainy.campos@gmail.com>
2023-09-20 17:11:29 +0100

    Load Profile component as default and all the api data related to it

--- src/components/User/Profile.js
+++ src/components/User/Profile.js
@@ -1,55 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import SelfIntroduction from "./SelfIntroduction";
 import UserInfo from "./UserInfo";
-import {getUser} from "../../api/api";
-import {USER_ID} from "../constants";
 
-const Profile = () => {
-
-    const [userInfo, setUserInfo] = useState()
-    const fetchUser = async () => {
-        try {
-            const response = await getUser(USER_ID);
-            const data = response.data;
-
-            const selfIntroduction = data.selfIntroduction || '';
-
-            // Assuming hobbyAndInterests is an array of strings
-            const interests = data.hobbyAndInterests || [];
-
-            const placesToVisit = data.placesToVisit ? [data.placesToVisit] : [];
-
-            const occupation = data.occupation ? [data.occupation] : [];
-
-            const hometown = data.hometown ? [data.hometown.city, data.hometown.country] : [];
-
-            setUserInfo({
-                selfIntroduction,
-                interests,
-                placesToVisit,
-                occupation,
-                hometown,
-            });
-        } catch (error) {
-            alert(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchUser();
-    }, []);
-
+const Profile = ({userInfo}) => {
     return (
         <>
-            {userInfo !== undefined &&
-                <>
-                    <SelfIntroduction selfIntroduction={userInfo.selfIntroduction}/>
-                    <UserInfo sectionHeader={'My Hometown'} list={userInfo.hometown}/>
-                    <UserInfo sectionHeader={'My Occupation'} list={userInfo.occupation}/>
-                    <UserInfo sectionHeader={'Interest & Hobbies'} list={userInfo.interests}/>
-                    <UserInfo sectionHeader={'Places I want to go'} list={userInfo.placesToVisit}/>
-                </>
-            }
+            <SelfIntroduction selfIntroduction={userInfo.selfIntroduction}/>
+            <UserInfo sectionHeader={'My Hometown'} list={userInfo.hometown}/>
+            <UserInfo sectionHeader={'My Occupation'} list={userInfo.occupation}/>
+            <UserInfo sectionHeader={'Interest & Hobbies'} list={userInfo.interests}/>
+            <UserInfo sectionHeader={'Places I want to go'} list={userInfo.placesToVisit}/>
         </>
     );
 }
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,22 +1,33 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import UserBanner from "./UserBanner";
 import Profile from "../User/Profile";
 import Moments from "../Moments/Moments";
 import UserTabs from "./UserTabs";
+import {getUser} from "../../api/api";
+import {USER_ID} from "../constants";
+import avatarImage from '../../images/avatar.png'
 
 const User = () => {
-    const user = {
-        name: 'User 1',
-        avatar: 'url_to_avatar_image',
-        latestMessage: 'Hello, this is the latest message from user 1',
-    };
 
-    const [component, setComponent] = useState(<Profile/>);
+    const [userInfo, setUserInfo] = useState({
+        name: '',
+        avatar: '',
+        latestMessage: '',
+        selfIntroduction: [],
+        hobbyAndInterests: [],
+        interests: [],
+        placesToVisit: [],
+        occupation: [],
+        hometown: [],
+    });
+    const [component, setComponent] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(true)
 
     const loadComponent = (componentName) => {
         switch (componentName) {
             case 'profile':
-                setComponent(<Profile/>);
+                setComponent(<Profile userInfo={userInfo}/>);
                 break;
             case 'moments':
                 setComponent(<Moments/>);
@@ -26,11 +37,43 @@ const User = () => {
         }
     }
 
+    const fetchUser = async () => {
+        try {
+            const response = await getUser(USER_ID);
+            const data = response.data;
+
+            setUserInfo({
+                selfIntroduction: data.selfIntroduction,
+                interests: data.hobbyAndInterests,
+                placesToVisit: data.placesToVisit ? [data.placesToVisit] : [],
+                occupation: data.occupation ? [data.occupation] : [],
+                hometown: data.hometown ? [data.hometown.city, data.hometown.country] : [],
+                name: data.name,
+                avatar: data.avatar || avatarImage,
+                latestMessage: data.latestMessage,
+            });
+
+            setIsLoading(false)
+
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchUser();
+        setComponent(<Profile userInfo={userInfo}/>)
+    }, [isLoading]);
+
     return (
         <>
-            <UserBanner user={user}/>
-            <UserTabs loadComponent={loadComponent}/>
-            {component}
+            {userInfo &&
+                <>
+                    <UserBanner user={userInfo}/>
+                    <UserTabs loadComponent={loadComponent} userInfo={userInfo}/>
+                    {component}
+                </>
+            }
         </>
     );
 }

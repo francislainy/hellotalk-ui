fd1df201a9a0c76d6e148004d3b717b0a613c2de
Francislainy <francislainy.campos@gmail.com>
2023-09-20 14:54:10 +0100

    Retrieve user info from api

--- src/components/User/Profile.js
+++ src/components/User/Profile.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SelfIntroduction from "./SelfIntroduction";
 import UserInfo from "./UserInfo";
+import {getUser} from "../../api/api";
+import {USER_ID} from "../constants";
 
 const Profile = () => {
 
+    const [userInfo, setUserInfo] = useState()
+    const fetchUser = async () => {
+        try {
+            const response = await getUser(USER_ID);
+            const data = response.data;
+
+            // Assuming hobbyAndInterests is an array of strings
+            const interests = data.hobbyAndInterests || [];
+
+            const placesToVisit = data.placesToVisit ? [data.placesToVisit] : [];
+
+            const occupation = data.occupation ? [data.occupation] : [];
+
+            const hometown = data.hometown ? [data.hometown.city, data.hometown.country] : [];
+
+            const school = data.school ? [data.school] : [];
+
+            setUserInfo({
+                interests,
+                placesToVisit,
+                occupation,
+                hometown,
+                school
+            });
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
     return (
         <>
-            <SelfIntroduction/>
-            <UserInfo sectionHeader={'Interest & Hobbies'} list={['item1', 'item2', 'item3']}/>
-            <UserInfo sectionHeader={'Places I want to go'} list={['place1', 'place2', 'place3']}/>
-            <UserInfo sectionHeader={'My Occupation'} list={['occupation1', 'occupation2', 'occupation3']}/>
-            <UserInfo sectionHeader={'My Hometown'} list={['hometown1', 'hometown2', 'hometown3']}/>
-            <UserInfo sectionHeader={'My School'} list={['school1', 'school2', 'school3']}/>
-            <h2>Age</h2>
+            {userInfo !== undefined &&
+                <>
+                    <SelfIntroduction/>
+                    <UserInfo sectionHeader={'Interest & Hobbies'} list={userInfo.interests}/>
+                    <UserInfo sectionHeader={'Places I want to go'} list={userInfo.placesToVisit}/>
+                    <UserInfo sectionHeader={'My Occupation'} list={userInfo.occupation}/>
+                    <UserInfo sectionHeader={'My Hometown'} list={userInfo.hometown}/>
+                    <UserInfo sectionHeader={'My School'} list={userInfo.school}/>
+                    <h2>Age</h2>
+                </>
+            }
         </>
     );
 }

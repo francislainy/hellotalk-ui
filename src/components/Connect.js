518c4f9fd76fb7743f8370f0f9893e313e45c468
Francislainy <francislainy.campos@gmail.com>
2023-09-26 10:14:50 +0100

    Show list of users coming from the api

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,36 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import UserCard from '../components/User/UserCard';
 import {USER_ID} from "./constants";
+import {getUsers} from "../api/api";
 
 const Connect = () => {
+    const [users, setUsers] = useState([])
 
-    const users = [
-        {
-            id: 1,
-            name: 'User 1',
-            description: 'This is user 1',
-            avatar: 'https://example.com/avatar1.png'
-        },
-        {
-            id: 2,
-            name: 'User 2',
-            description: 'This is user 2',
-            avatar: 'https://example.com/avatar2.png'
-        },
-        {
-            id: 3,
-            name: 'User 3',
-            description: 'This is user 3',
-            avatar: 'https://example.com/avatar3.png'
-        },
-        {
-            id: 4,
-            name: 'User 4',
-            description: 'This is user 4',
-            avatar: 'https://example.com/avatar4.png'
-        },
-        // Add more users as needed
-    ];
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await getUsers();
+                setUsers(response.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchUsers()
+    }, [])
 
     return (
         <div>

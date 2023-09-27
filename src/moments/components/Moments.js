d66cd27a2f9e6616f7b8c1c95b5f482e25b13636
Francislainy <francislainy.campos@gmail.com>
2023-09-27 16:22:43 +0100

    Move moments code to moments screen

--- src/moments/components/Moments.js
+++ src/moments/components/Moments.js
@@ -3,19 +3,13 @@ import {Box} from "@mui/material";
 
 import useMoments from '../hooks/useMoments';
 import MomentList from "./MomentList";
-import FabAddMoment from "./FabAddMoment";
-import MomentFilterSelector from "./MomentFilterSelector";
 
 const Moments = () => {
-    const { moments, fetchMoments } = useMoments('');
+    const {moments} = useMoments('');
 
     return (
-        <Box sx={{display: 'grid', justifyContent: 'center', marginTop: '20px'}}>
-            <MomentFilterSelector fetchMoments={fetchMoments}/>
-            <Box sx={{width: '100%', alignSelf: 'start'}}>
-                <MomentList moments={moments} style={{width: '100%'}}/>
-            </Box>
-            <FabAddMoment/>
+        <Box sx={{width: '100%', alignSelf: 'start'}}>
+            <MomentList moments={moments} style={{width: '100%'}}/>
         </Box>
     );
 }
--- src/moments/screens/MomentsScreen.js
+++ src/moments/screens/MomentsScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import Moments from "../components/Moments";
+import {Box} from "@mui/material";
 
-const MomentsScreen = () => {
-  return (
-    <div>
-      <Moments />
-    </div>
-  );
+import useMoments from '../hooks/useMoments';
+import MomentFilterSelector from "../components/MomentFilterSelector";
+import MomentList from "../components/MomentList";
+import FabAddMoment from "../components/FabAddMoment";
+
+const Moments = () => {
+    const { moments, fetchMoments } = useMoments('');
+
+    return (
+        <Box sx={{display: 'grid', justifyContent: 'center', marginTop: '20px'}}>
+            <MomentFilterSelector fetchMoments={fetchMoments}/>
+            <Box sx={{width: '100%', alignSelf: 'start'}}>
+                <MomentList moments={moments} style={{width: '100%'}}/>
+            </Box>
+            <FabAddMoment/>
+        </Box>
+    );
 }
-
-export default MomentsScreen;
+export default Moments;

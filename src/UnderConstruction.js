b10872ceb6ceffbe918c820e291d54e5fd701b3e
Francislainy <francislainy.campos@gmail.com>
2023-11-08 13:18:31 +0000

    Update under construction message

--- src/UnderConstruction.js
+++ src/UnderConstruction.js
@@ -7,7 +7,7 @@ const UnderConstruction = ({title}) => {
             <div style={{display: 'flex', alignItems: 'center', height: '100vh', flexDirection: 'column'}}>
                 <h1 style={{fontSize: '2em', color: '#7B1FA2'}}>Under Construction</h1>
                 <p style={{fontSize: '1.2em', color: '#555'}}>This section is currently being developed. Please check
-                    back later.</p>
+                    again later.</p>
             </div>
         </div>
     );

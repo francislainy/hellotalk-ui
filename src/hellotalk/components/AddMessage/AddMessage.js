c7bfd2e5bbd075e839cdc144941537f9f0de12c4
Francislainy <francislainy.campos@gmail.com>
2023-11-08 13:57:35 +0000

    Turn chat sections into components

---
+++ src/hellotalk/components/AddMessage/AddMessage.js
@@ -0,0 +1,31 @@
+import {Button, TextField} from "@mui/material";
+import colors from "../../../colors/colors";
+import React from "react";
+
+const AddMessage = ({handleNewMessageChange, newMessage, handleNewMessageSubmit}) => {
+    return (<form onSubmit={handleNewMessageSubmit} sx={{marginTop: 2}}>
+            <TextField
+                value={newMessage}
+                onChange={handleNewMessageChange}
+                label="New Message"
+                variant="outlined"
+                sx={{width: '75%', marginBottom: 2}}
+            />
+            <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                    marginLeft: 1,
+                    backgroundColor: colors.primary,
+                    '&:hover': {
+                        backgroundColor: colors.darkPurple, // darker shade for hover effect
+                    }
+                }}
+            >
+                Send
+            </Button>
+        </form>
+    )
+}
+
+export default AddMessage;
--- src/hellotalk/components/Chat/Chat.js
+++ src/hellotalk/components/Chat/Chat.js
@@ -1,10 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {Avatar, Button, List, ListItem, ListItemAvatar, ListItemText, TextField} from '@mui/material';
-import avatarImage from '../../../images/avatar.png'
 import ChatDetails from "../ChatDetails/ChatDetails";
-import colors from "../../../colors/colors";
 import {getChats} from "../../../api/api";
-import {USER_ID} from "../../../constants/constants";
+import AddMessage from "../AddMessage/AddMessage";
+import ChatList from "../ChatList/ChatList";
 
 const ChatInterface = () => {
     // Assuming you have a list of chat data
@@ -42,53 +40,18 @@ const ChatInterface = () => {
 
     return (
         <div style={{display: 'flex'}}>
-            <List>
-                {chats.map((chat, index) => {
-                    // Find the participant who is not the current user
-                    const otherParticipant = chat.participants.find(participant => participant.id !== USER_ID);
-                    return (
-                        <ListItem
-                            key={index}
-                            button
-                            selected={selectedChat === index}
-                            onClick={() => handleChatItemClick(index)}
-                        >
-                            <ListItemAvatar>
-                                <Avatar src={avatarImage} sx={{width: 50, height: 50}}/>
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary={otherParticipant ? otherParticipant.id : 'Default Value'}
-                                secondary={chat.messages[0].content} // Most recent message
-                                sx={{fontSize: '2.5em'}} // increase the font size
-                            />
-                        </ListItem>
-                    );
-                })}
-            </List>
+            <ChatList
+                chats={chats}
+                selectedChat={selectedChat}
+                handleChatItemClick={handleChatItemClick}
+            />
             <div style={{flex: 2, marginLeft: '60px'}}>
                 <ChatDetails chat={chats[selectedChat]}/>
-                <form onSubmit={handleNewMessageSubmit} sx={{marginTop: 2}}>
-                    <TextField
-                        value={newMessage}
-                        onChange={handleNewMessageChange}
-                        label="New Message"
-                        variant="outlined"
-                        sx={{width: '75%', marginBottom: 2}}
-                    />
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        sx={{
-                            marginLeft: 1,
-                            backgroundColor: colors.primary,
-                            '&:hover': {
-                                backgroundColor: colors.darkPurple, // darker shade for hover effect
-                            }
-                        }}
-                    >
-                        Send
-                    </Button>
-                </form>
+                <AddMessage
+                    handleNewMessageChange={handleNewMessageChange}
+                    newMessage={newMessage}
+                    handleNewMessageSubmit={handleNewMessageSubmit}
+                />
             </div>
         </div>
     );

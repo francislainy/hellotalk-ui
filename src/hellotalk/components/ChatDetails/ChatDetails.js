75a855cd2689d5a6b7b2459915949f6999c80d42
Francislainy <francislainy.campos@gmail.com>
2024-02-13 16:53:28 +0000

    Clean up on chat details component

--- src/hellotalk/components/ChatDetails/ChatDetails.js
+++ src/hellotalk/components/ChatDetails/ChatDetails.js
@@ -4,28 +4,35 @@ import {USER_ID} from "../../../constants/constants";
 import ChatContext from '../../contexts/ChatContext';
 
 const ChatDetails = () => {
-    const {
-        chat,
-    } = useContext(ChatContext);
+    const { chat } = useContext(ChatContext);
 
     if (!chat) {
         return <div>No chat selected</div>;
     }
 
-    const otherParticipant = chat.participants.find(participant => participant.id !== USER_ID);
+    const otherParticipant = getOtherParticipant(chat);
+    const messages = getChatMessages(chat, otherParticipant);
 
     return (
         <div>
-            {(chat.messages.length > 0) && (
-                <div>
-                    <h2>{chat.messages[0] ? otherParticipant.name : "No messages in chat"}</h2>
-                    {chat.messages.map((message, index) => (
-                        <MessageItem message={message} index={index}/>
-                    ))}
-                </div>
-            )}
+            {messages}
         </div>
     );
 };
+
+const getOtherParticipant = (chat) => {
+    return chat.participants.find(participant => participant.id !== USER_ID);
+}
+
+const getChatMessages = (chat, otherParticipant) => {
+    return chat.messages.length > 0 && (
+        <div>
+            <h2>{chat.messages[0] ? otherParticipant.name : "No messages in chat"}</h2>
+            {chat.messages.map((message, index) => (
+                <MessageItem message={message} index={index}/>
+            ))}
+        </div>
+    );
+}
 
 export default ChatDetails;

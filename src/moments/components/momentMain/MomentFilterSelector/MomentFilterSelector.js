041db525ed45d43ef9ac76c882e1efced1e655c1
Francislainy <francislainy.campos@gmail.com>
2024-02-16 12:25:01 +0000

    CSS for moment filter screen moved to its own file. Filter styling for fab button.

--- src/moments/components/momentMain/MomentFilterSelector/MomentFilterSelector.js
+++ src/moments/components/momentMain/MomentFilterSelector/MomentFilterSelector.js
@@ -1,37 +1,37 @@
 import React, {useContext} from 'react';
 import {Box, Button} from "@mui/material";
-import colors from '../../../colors/colors';
-import MomentContext from "../../contexts/MomentContext";
+import MomentContext from "../../../contexts/MomentContext";
+import './MomentFilterSelector.css';
 
 const BUTTONS = [
-    { name: 'Recent', param: '' },
-    { name: 'Best', param: 'best_param' },
-    { name: 'Learn', param: 'best_param' },
-    { name: 'Nearby', param: 'best_param' },
-    { name: 'Following', param: 'best_param' },
-    { name: 'Help', param: 'best_param' },
-    { name: 'Classmates', param: 'best_param' },
-    { name: 'Voice', param: 'best_param' },
+    {name: 'Recent', param: ''},
+    {name: 'Best', param: 'best_param'},
+    {name: 'Learn', param: 'best_param'},
+    {name: 'Nearby', param: 'best_param'},
+    {name: 'Following', param: 'best_param'},
+    {name: 'Help', param: 'best_param'},
+    {name: 'Classmates', param: 'best_param'},
+    {name: 'Voice', param: 'best_param'},
 ];
 
 const MomentFilterSelector = () => {
     const {fetchMoments} = useContext(MomentContext);
 
     return (
-        <Box sx={{'& > :not(style)': {m: 1}, display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+        <Box className="moment-filter-selector">
             {BUTTONS.map((button) => (
                 <Button
                     key={button.name}
                     variant="contained"
-                    sx={{color: colors.white, backgroundColor: colors.primary}}
+                    className="moment-filter-button"
                     onClick={() => fetchMoments(button.param)}
-                    TouchRippleProps={{style: {color: '#7B1FA2'}}}
                 >
                     {button.name}
                 </Button>
             ))}
         </Box>
-    )
+    );
 };
 
 export default MomentFilterSelector;
+
--- src/moments/screens/MomentsScreen.js
+++ src/moments/screens/MomentsScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Box} from "@mui/material";
-import MomentFilterSelector from "../components/momentMain/MomentFilterSelector";
+import MomentFilterSelector from "../components/momentMain/MomentFilterSelector/MomentFilterSelector";
 import MomentList from "../components/momentMain/MomentList/MomentList";
 import FabAddMoment from "../components/momentMain/FabAddMoment/FabAddMoment";
 import MomentProvider from "../provider/MomentProvider";
